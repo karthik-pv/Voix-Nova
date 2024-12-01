378f75f9c45cab20a91443c50252c545cfa0070b
goutham <goutham3336@gmail.com>
2024-12-01 12:31:56 +0530

    new changes

--- core/urls.py
+++ core/urls.py
@@ -10,7 +10,7 @@ from .views import (
     home_page_conversationalist,
     product_list_page_conversationalist,
     filter_conversationalist,
-    product_details_page_conversationalist,
+    product_details_page_conversationalist, image_similarity_view,
 )
 
 urlpatterns = [
@@ -34,6 +34,7 @@ urlpatterns = [
         product_details_page_conversationalist,
         name="product_details_page_conversationalist",
     ),
+path('image-similarity/', image_similarity_view, name='image_similarity'),
     path("api/get_all_products/", get_all_products, name="get_all_products"),
     path("api/group_search/", group_search_view, name="group_search"),
     path("api/particular_search/", particular_search_view, name="particular_search"),
